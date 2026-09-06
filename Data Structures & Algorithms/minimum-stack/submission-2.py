60ae8ff3e38902a5d512514d96f5e5f6c0f2e7b0
Leo Barry <github.q56mq@simplelogin.com>
2026-09-05 17:48:05 -0700

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.py
@@ -0,0 +1,22 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = deque()
+        self.min_stack = deque()
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if self.min_stack:
+            val = min(val, self.min_stack[-1])
+        
+        self.min_stack.append(val)
+
+    def pop(self) -> None:
+        self.min_stack.pop()
+        return self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min_stack[-1]
